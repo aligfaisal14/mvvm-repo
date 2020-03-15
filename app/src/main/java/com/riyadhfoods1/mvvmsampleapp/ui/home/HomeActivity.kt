5c9e74dfa2e0a462237bf108e6c2f0eaf61476f9
RiyadhFoods <falkhotaifi.rff@gmail.com>
2020-03-15 13:27:23 +0300

    1.Kodein dependency injection 2.SignUp Activity and viewmodel working 3.HomeActivity with fragment and navigation drawer

--- app/src/main/java/com/riyadhfoods1/mvvmsampleapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/riyadhfoods1/mvvmsampleapp/ui/home/HomeActivity.kt
@@ -2,12 +2,29 @@ package com.riyadhfoods1.mvvmsampleapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.riyadhfoods1.mvvmsampleapp.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+        val navigation = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view,navigation)
+        NavigationUI.setupActionBarWithNavController(this,navigation,drawerLayout)
+
+
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.fragment), drawerLayout)
+    }
+
 }
---
+++ app/src/main/java/com/riyadhfoods1/mvvmsampleapp/ui/home/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.riyadhfoods1.mvvmsampleapp.ui.home.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
